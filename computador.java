5dda02b6471e1c1b242e6cc8e11304f928a278f4
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-03-12 18:19:09 -0500

    Update computador.java

--- computador.java
+++ computador.java
@@ -1,114 +1,133 @@
+
 package ClasesYObjetos;
 
-public class computador
+public class zoologico 
 {
     //atributos
-    String fotogramas;
-    String generacion;
+    String nombre;
+    String descripcionengeneral;
+    String habitad;
+    String peso;
+    String clima;
+    String tipodevegetacion;
+    String terrestres;
+    String acuaticos; 
+    String aeroterrestres;
+    String direccion;
+    String telefono;
+    String fechadeingreso;
+    String cargodeespecie;
+    String nombredezona;
+    String extencionqueocupa;
+    String lugaresdelzoologico;
     String marca;
-    String tamaño;
-    String color;
-    String teclas;
-    String fuentedepoder;
-    String ventilador;
-    String tipodeteclado;
-    String pantalla;
-    String base;
-    String cubierta;
-    String cable;
-    String tiposdemouse;
-    String tipodetorre;
-    String resolucion;
+    String tipodeespecie;
+    String fechadeiniciodetrabajo;
+    String itinerarios;
+    String horas;
+    String codigodeitinerario;
+    String duraciondelrecorrido;
+    String longituddelitinerario;
+    String visitantesautorizados;
+    String zonaderecorrido;
     
     
-    public static void main (String [] args)
-    {
-        computador monitor = new computador ();
-        
-        monitor.color=" gris";
-        monitor.pantalla="LCD ";
-        monitor.base=" mesa ";
-        monitor.marca="asus";
-        
-        
-        System.out.println("el color del monitor: "+monitor.color);
-        System.out.println("la pantalla del monitor: "+monitor.pantalla);
-        System.out.println("la base del monitor: "+monitor.base);
-        System.out.println("la marca del monitor: "+monitor.marca);
-                
-                
-        computador torre = new computador ();
-        
-        torre.color="negro";
-        torre.tipodetorre="micro-ATX";
-        torre.ventilador="CNC disparadores maquinados";
-        torre.fuentedepoder="ATX ";
-        
-        System.out.println("el color de la torre es: "+torre.color);
-        System.out.println("el tipo de la torre es: "+torre.tipodetorre);
-        System.out.println("el ventilador de la torre es: "+torre.ventilador);
-        System.out.println("la fuente de poder de la torre es: "+torre.fuentedepoder);
-        
-        
-        
-        computador raton = new computador ();
-        
-        raton.cable=" puertos PS/2";
-        raton.color="negro con blanco";
-        raton.cubierta="gaming";
-        raton.tiposdemouse="gaming";
-       
-        System.out.println("el cable del raton es: "+raton.cable);
-        System.out.println("el color del raton es: "+raton.color);
-        System.out.println("la cubierta del raton es: "+raton.cubierta);
-        System.out.println("el tipo de raton es: "+raton.tiposdemouse);
-        
-        computador teclado = new computador ();
-        
-        teclado.color="blanco";
-        teclado.teclas="alfanumericas";
-        teclado.generacion="";
-        teclado.tamaño="17.5 pulgadas y 6 pulgadas de profundidad";
-        teclado.marca="corsair";
-        teclado.tipodeteclado=" proyeccion";
-        
-        
-        System.out.println("el color del teclado es: "+teclado.color);
-        System.out.println("las teclas del teclado son : "+teclado.teclas);
-        System.out.println("la generacion del teclado es: "+teclado.generacion);
-        System.out.println("el tamaño del teclado es: "+teclado.tamaño);
-        System.out.println("la marca del teclado es: "+teclado.marca);
-        System.out.println("el tipo de teclado es: "+teclado.tipodeteclado);
-        
-        
-        computador camara = new computador ();
-        
-        camara.color="negro";
-        camara.resolucion="5.0 megapixeles";
-        camara.generacion="1991";
-        camara.fotogramas="normal";
-        
-        System.out.println("el color de la camara es: "+camara.color);
-        System.out.println("la resolucion de la camara es: "+camara.resolucion);
-        System.out.println("la generacion de la camara es: "+camara.generacion);
-        System.out.println("el fotograma de la camara es: "+camara.fotogramas);
-        
-        
-        {
-            String cantidaddediscosduros;
-            String memoria;
-            String marca;
-            String 
-        }
-        
-        
+ public static void main (String []args )
+ {
+     zoologico especies = new zoologico();
      
-        
-    }
+     especies.nombre= "jirafa";
+     especies.descripcionengeneral= "Este rumiante africano es el animal más alto conocido, llegando a alcanzar los 6 metros de altura. ";
+     especies.habitad= "africa";
+     especies.peso= "800 - 1.900KG ";
+     
+     System.out.println("\nEL NOMBRE DE LA ESPECIE ES : "+especies.nombre);
+     System.out.println("LA DESCRIPCION DE LA ESPECIE ES : "+especies.descripcionengeneral);
+     System.out.println("EL HABITAD DE LA ESPECIE ES : "+especies.habitad);
+     System.out.println("EL PESO DE LA ESPECIE ES : "+especies.peso);
+     
+     
+     zoologico habitad = new zoologico();
+     
+     habitad.nombre="africa,sabanas";
+     habitad.clima="temperatura templada ";
+     habitad.tipodevegetacion="herbivoros";
+   
+     
+     System.out.println("\nEL NOMBRE DE SU HABITAD ES: "+habitad.nombre);
+     System.out.println("EL CLIMA DE SU HABITAD ES: "+habitad.clima);
+     System.out.println("LA VEGETACION DE SU HABITAD ES: "+habitad.tipodevegetacion);
 
+     
+     zoologico zonas = new zoologico();
+     
+     zonas.nombredezona="jirafas";
+     zonas.extencionqueocupa="";
+     zonas.lugaresdelzoologico=" safaris africanos";
+     
+     System.out.println("\nEL NOMBRE DE LA ZONA ES: "+zonas.nombredezona);
+     System.out.println("LA EXTENCION QUE OCUPA LA ZONA ES:  "+zonas.extencionqueocupa);
+     System.out.println("LUGAR DEL ZOOLOGICO: "+zonas.lugaresdelzoologico);
 
+     
+     zoologico cuidadores = new zoologico();
+     
+     cuidadores.nombre=" angie ";
+     cuidadores.direccion="cal 92A #87-54 NORTE";
+     cuidadores.telefono="3204587455";
+     cuidadores.fechadeingreso="12-05-2020";
+     cuidadores.cargodeespecie="jirafas";
+     
+     System.out.println("\nEL NOMBRE DEL CUIDADOR ES: "+cuidadores.nombre);
+     System.out.println("ALA DIRECCION DEL CUIDADOR ES: "+cuidadores.direccion);
+     System.out.println("EL TELEFONO DEL CUIDADOR ES: "+cuidadores.telefono);
+     System.out.println("LA FECHA DE INGRESO DEL CUIDADOR ES : "+cuidadores.fechadeingreso);
+     System.out.println("EL CARGO DE LA ESPECIE ES: "+cuidadores.cargodeespecie);
 
-
+     
+     zoologico itinerarios = new zoologico();
+     
+     itinerarios.codigodeitinerario="FXP,FXX";
+     itinerarios.duraciondelrecorrido=" 1 HORA Y MEDIA";
+     itinerarios.longituddelitinerario="";
+     itinerarios.visitantesautorizados="225";
+     itinerarios.zonaderecorrido="";
+     
+     System.out.println("\nEL CODIGO DEL ITINERARIO ES: "+itinerarios.codigodeitinerario);
+     System.out.println("LA DURACION DEL RECORRIDO ES: "+itinerarios.duraciondelrecorrido);
+     System.out.println("LA LONGITUD DEL ITINERARIO ES: "+itinerarios.longituddelitinerario);
+     System.out.println("LOS VISITANTES AUTORIZADOS SON : "+itinerarios.visitantesautorizados);
+     System.out.println("LA ZONA DEL RECORRIDO ES: "+itinerarios.zonaderecorrido);
+     
+     zoologico guias = new zoologico();
+     
+     guias.nombre="guia 1";
+     guias.direccion="cal 65# 85-78A";
+     guias.telefono="3204867845";
+     guias.fechadeiniciodetrabajo="12-03-2020";
+     guias.itinerarios="";
+     guias.horas="8hr";
     
-
-}
+     System.out.println("\nEL NOMBRE DE LA GUIA ES: "+guias.nombre);
+     System.out.println("ALA DIRECCION DE LA GUIA ES: "+guias.direccion);
+     System.out.println("EL TELEFONO ES: "+guias.telefono);
+     System.out.println("LA FECHA DE INICIO DEL TRABAJO ES : "+guias.fechadeiniciodetrabajo);
+     System.out.println("EL ITINERARIO ES : "+guias.itinerarios);
+     System.out.println("LAS HORAS DE GUIA SON : "+guias.horas);
+     
+     
+     
+     zoologico alimentos = new zoologico();
+     
+     alimentos.marca="hojas de acacia";
+     alimentos.tipodeespecie="jirafa";
+     
+     System.out.println("LA MARCA DEL ALIMENTO ES: "+alimentos.marca);
+     System.out.println("EL TIPO DE ANIMAL ES: "+alimentos.tipodeespecie);
+     
+     
+     
+     
+     
+     
+ }
