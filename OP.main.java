233ce8949d73b2ec9c7da8634114e64dda24e8f8
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-03-13 10:11:17 -0500

    Create OP.main.java

---
+++ OP.main.java
@@ -0,0 +1,20 @@
+
+package CLASESYOBJETOS;
+
+
+public class Main 
+{
+    public static void main ( String []args)
+    {
+        Operacion op = new Operacion ();
+        
+        op.leerNumeros();
+        op.sumar();
+        op.restar();
+        op.multiplicacion();
+        op.dividir();
+        op.porcentaje();
+        op.mostrarResultado();
+        
+    }
+}
