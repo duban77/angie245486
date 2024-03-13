44734e628846ecdd179e7c2bc16f52c510cb211d
angie245486 <162470367+angie245486@users.noreply.github.com>
2024-03-13 13:26:48 -0500

    Create vectores1.cpp

---
+++ vectores1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int v [5];  //0,1,2,3,4
+	int i;
+	
+	for (i=0; i<=4; i=i+1){
+		cout<<" introduzca un numero: ";
+		cin>>v[i];
+	}
+	
+	
+	for (i=0 ; i<=4; i=i+1){
+		cout<< v [i]<<"   ";
+	
+}
+}
