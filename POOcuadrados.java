d45aae1a22225247d08adfd14239959a5dd292fc
duban77 <159207588+duban77@users.noreply.github.com>
2024-03-14 09:27:52 -0500

    Create POOcuadrados.java

---
+++ POOcuadrados.java
@@ -0,0 +1,32 @@
+package Cuadrados;
+import javax.swing.*;
+public class Lados_de_un_cuadrado 
+{
+    int lado1;
+    int lado2;
+    int lado3;
+    int lado4;
+    int area;
+    float perimetro;
+    
+    public void LeerNumeros1()
+    {
+        lado1=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+        lado2=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+        lado3=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+        lado4=Integer.parseInt(JOptionPane.showInputDialog("Digite un numero: "));
+    }
+        public void area()
+        {
+            area= lado1*lado2*lado3*lado4;
+        }
+        public void perimetro()
+        {
+            perimetro= lado1+lado2+lado3+lado4;
+        }
+        public void mostrarResultado()
+     {
+         System.out.println("el area es :  "+area);
+         System.out.println("el perimetro es :  "+perimetro);
+     }
+}
